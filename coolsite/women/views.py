b524c4383b86e655877448870e3bd156d3c92dc0
a.sotnichenko <haliivi@mail.ru>
2022-07-25 16:29:30 +0700

    First commit.

---
+++ coolsite/women/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('Страница приложения women.')
+
+
+def categories(requeest, catid):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
